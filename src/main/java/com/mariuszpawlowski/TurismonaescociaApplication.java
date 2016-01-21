3a01bda4bd93526a118a82a2adaa3575106e7cf9
Mariusz Pawlowski <mariusz.pawlowski@dynatrace.com>
2016-01-21 16:59:21 +0100

    initial commit

---
+++ src/main/java/com/mariuszpawlowski/TurismonaescociaApplication.java
@@ -0,0 +1,12 @@
+package com.mariuszpawlowski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurismonaescociaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurismonaescociaApplication.class, args);
+	}
+}
